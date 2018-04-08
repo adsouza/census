a429158ac7eb346d7b515c42ecb7042141a4c71c
Antonio D'souza <adsouza@gmail.com>
2018-04-08 15:18:07 -0400

    Parse form data
    Attempt to convert all incoming form values from the POST req to ints and treat failures as errors.

--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
 
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+
+	"github.com/hashicorp/go-multierror"
 )
 
 var (
 	indexTmpl = template.Must(template.ParseFiles("index.html"))
 )
+
+type People struct {
+	Total, Grouped, Solitary, Asleep int
+}
+
+type Snapshot struct {
+	People
+	Decibels, Laptops int
+}
+
+func reportError(ctx context.Context, msg string, w http.ResponseWriter) {
+	w.WriteHeader(http.StatusBadRequest)
+	log.Errorf(ctx, msg)
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
@@ -18,7 +38,33 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == http.MethodPost {
-		r.FormValue("total")
+		ctx := appengine.NewContext(r)
+		record := Snapshot{}
+		var badness *multierror.Error
+		var err error
+		if record.Total, err = strconv.Atoi(r.FormValue("total")); err != nil {
+			badness = multierror.Append(badness, err)
+		}
+		if record.Grouped, err = strconv.Atoi(r.FormValue("grouped")); err != nil {
+			badness = multierror.Append(badness, err)
+		}
+		if record.Solitary, err = strconv.Atoi(r.FormValue("solitary")); err != nil {
+			badness = multierror.Append(badness, err)
+		}
+		if record.Asleep, err = strconv.Atoi(r.FormValue("asleep")); err != nil {
+			badness = multierror.Append(badness, err)
+		}
+		if badness.ErrorOrNil() != nil {
+			msg := fmt.Sprintf("Failure parsing numbers: %v.", badness)
+			reportError(ctx, msg, w)
+			return
+		}
+		if record.Total != record.Grouped+record.Solitary+record.Asleep {
+			msg := fmt.Sprintf("Total (%d) != grouped (%d) + solitary (%d) + asleep (%d).",
+				record.Total, record.Grouped, record.Solitary, record.Asleep)
+			reportError(ctx, msg, w)
+			return
+		}
 	}
 
 	indexTmpl.Execute(w, nil)
